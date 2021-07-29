fa09553e441840289d32745fad5b9dba9f253ab5
Leo Kim <me@leok.kr>
2021-07-29 16:11:14 +0900

    feat: move object constructor to models.py and removed _get_AnimeInfo_by_rawdata

--- laftel/laftel.py
+++ laftel/laftel.py
@@ -9,42 +9,8 @@ class ApiUrl:
     SearchAnime = "https://laftel.net/api/search/v1/keyword/?keyword={query}"
 
 
-async def _get_AnimeInfo_by_rawdata(data: dict):
-    obj = AnimeInfo(data)
-    obj.url = f'https://laftel.net/item/{data.get("id", None)}'
-    obj.id = data.get("id", None)
-    obj.name = data.get("name", None)
-    obj.image = data.get("img", None)
-    obj.content = data.get("content", None)
-    obj.ended = data.get("is_ending", None)
-    obj.awards = data.get("awards", None)
-
-    obj.content_rating = data.get("content_rating", None)
-    obj.adultonly = data.get("is_adult", None)
-    obj.viewable = data.get("viewable", None)
-    obj.genres = data.get("genres", None)
-    obj.tags = data.get("tags", None)
-
-    _animation_info = data.get("animation_info", {})
-    obj.air_year_quarter = _animation_info.get("air_year_quarter", None)
-    obj.air_day = (
-        _animation_info.get("original_air_time", None)
-        if _animation_info.get("original_air_time", None)
-        else _animation_info.get("distributed_air_time", None)
-    )
-    _meta_info = data.get("meta_info", {})
-    obj.avg_rating = _meta_info.get("avg_rating", None)
-    obj.view_male = _meta_info.get("male", None)
-    obj.view_female = _meta_info.get("female", None)
-    if not obj.id:
-        return None
-    return obj
-
-
 async def getAnimeInfo(id: int) -> AnimeInfo:
-    return await _get_AnimeInfo_by_rawdata(
-        await api_request(ApiUrl.AnimeItem.format(id=id))
-    )
+    return AnimeInfo(await api_request(ApiUrl.AnimeItem.format(id=id)))
 
 
 async def searchAnime(query: str, issync: bool = False) -> List[SearchResult]:
@@ -54,12 +20,6 @@ async def searchAnime(query: str, issync: bool = False) -> List[SearchResult]:
     for data in datas:
 
         obj = SearchResult(data)
-        obj.url = f'https://laftel.net/item/{data.get("id", None)}'
-        obj.id = data.get("id", None)
-        obj.name = data.get("name", None)
-        obj.image = data.get("img", None)
-        obj.adultonly = data.get("is_adult", None)
-        obj.genres = data.get("genres", None)
 
         def get_data():
             return (
--- laftel/models.py
+++ laftel/models.py
@@ -26,7 +26,32 @@ class AnimeInfo:
     view_female: int  # Percentage of woman in total watched user (여성 시청자 비율)
 
     def __init__(self, data):
-        self.rawdata = data
+        self.id = data.get("id")
+        self.name = data.get("name")
+        self.url = f"https://laftel.net/item/{self.id}"
+        self.image = data.get("img")
+        self.content = data.get("content")
+        self.ended = data.get("is_ending")
+        self.awards = data.get("awards")
+
+        self.content_rating = data.get("content_rating")
+        self.adultonly = data.get("is_adult")
+        self.viewable = data.get("viewable")
+        self.genres = data.get("genres")
+        self.tags = data.get("tags")
+
+        _animation_info = data.get("animation_info", {})
+        self.air_year_quarter = _animation_info.get("air_year_quarter")
+        self.air_day = (
+            _animation_info.get("original_air_time")
+            if _animation_info.get("original_air_time")
+            else _animation_info.get("distributed_air_time")
+        )
+
+        _meta_info = data.get("meta_info", {})
+        self.avg_rating = _meta_info.get("avg_rating")
+        self.view_male = _meta_info.get("male")
+        self.view_female = _meta_info.get("female")
 
 
 @dataclass(repr=True)
@@ -41,4 +66,17 @@ class SearchResult:
     get_data: Callable  # Function returns AnimeInfo for this anime (AnimeInfo 가져오는 함수)
 
     def __init__(self, data):
-        self.rawdata = data
+        self.id = data.get("id")
+        self.name = data.get("name")
+        self.url = f"https://laftel.net/item/{self.id}"
+        self.image = data.get("img")
+        self.adultonly = data.get("is_adult")
+        self.genres = data.get("genres")
+
+    def get_data():
+        """
+        It Will be Filled in searchAnime Function.
+        returns function "sync.getAnimeInfo" or Coroutine "getAnimeInfo".
+        You will need to await this function if you had searched by coroutine.
+        """
+        raise NotImplementedError
