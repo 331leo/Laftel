670a81d2b61d76c4ff6a7f29b18f2ab326b1c866
Leo Kim <me@leok.kr>
2023-06-20 15:10:31 +0900

    update init file

--- laftel/__init__.py
+++ laftel/__init__.py
@@ -1,4 +1,4 @@
-from .laftel import getAnimeInfo, searchAnime, sync
+from .laftel import getAnimeInfo, searchAnime, searchSeries, searchEpisodes, sync
 
-__all__ = ["sync", "searchAnime", "getAnimeInfo"]
-__version__ = "1.2.2"
+__all__ = ["sync", "searchAnime", "getAnimeInfo", "searchSeries", "searchEpisodes"]
+__version__ = "1.3.0"
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 setup(
     name="laftel",
-    version="1.2.2",
+    version="1.3.0",
     description="파이썬 라프텔 라이브러리, Unofficial Python Laftel API Wrapper(Laftel)",
     license="GPL-V3",
     author="LeoK",
